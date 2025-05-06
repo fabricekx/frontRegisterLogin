7c367cfe7df42284b2645579eda026725d06ddf1
fabricekx <fabricekx@gmail.com>
2025-05-06 14:46:39 +0200

    console log de debug

--- src/api/auth.js
+++ src/api/auth.js
@@ -2,8 +2,13 @@ import axios from 'axios';
 
 const API = axios.create({
     baseURL: "https://nestjs-login-registration-profile-production.up.railway.app",
-    // baseURL: "http://localhost:3000"
+    // baseURL: "http://localhost:3000",
+    withCredentials: true,
 });
 
-export const register = (data) => API.post('/auth/register', data);
-export const login = (data) => API.post('/auth/login', data);
+export const register = (data) => {
+  console.log("Sending data to backend:", data);  // Log les données envoyées
+  return API.post('/auth/register', data)
+    .then(response => console.log("Response from backend:", response))
+    .catch(error => console.error("Error sending request:", error));
+};export const login = (data) => API.post('/auth/login', data);
